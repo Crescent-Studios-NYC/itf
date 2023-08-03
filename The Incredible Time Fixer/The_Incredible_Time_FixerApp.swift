7f46a39da7299d6be075b7472f95f604406fbe06
Panagiotis Peikidis <pekalicious@gmail.com>
2023-08-03 07:21:15 -0400

    Initial Commit

---
+++ The Incredible Time Fixer/The_Incredible_Time_FixerApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Incredible_Time_FixerApp.swift
+//  The Incredible Time Fixer
+//
+//  Created by Panagiotis Peikidis on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct The_Incredible_Time_FixerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
